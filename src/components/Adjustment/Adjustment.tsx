e005123e3e8283e78d2a368039d996f59e2a3bcc
BaitemirAsanbaev <baitemir1706@gmail.com>
2024-11-15 02:10:53 +0600

    adjusstment block stylized

--- src/components/Adjustment/Adjustment.tsx
+++ src/components/Adjustment/Adjustment.tsx
@@ -9,6 +9,8 @@ import {
 } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { fetchCategories } from "../../store/services/categoryService";
+import classes from "./Adjustment.module.scss";
+
 export function Adjustment() {
   const [target, setTarget] = useState<string>("");
   const [filter, setFilter] = useState<string>("*");
@@ -16,39 +18,57 @@ export function Adjustment() {
   const dispatch = useAppDispatch();
   const categories = useAppSelector((state) => state.products.categories);
 
-  useEffect(()=>{
-    dispatch(fetchCategories())
-  }, [])
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, []);
 
   return (
-    <div>
-      <FormControl>
-        <InputLabel id="filter-label">Filter</InputLabel>
-        <Select
-          labelId="filter-label"
-          id="filter"
-          value={filter}
-          label="Filter"
-          onChange={(e) => {
-            setFilter(e.target.value.toString());
-          }}
+    <div className={classes.Adjustment}>
+      <div>
+        <FormControl>
+          <InputLabel id="filter-label">Filter</InputLabel>
+          <Select
+            labelId="filter-label"
+            id="filter"
+            value={filter}
+            label="Filter"
+            onChange={(e) => {
+              setFilter(e.target.value.toString());
+            }}
+          >
+            <MenuItem value={"*"}>All</MenuItem>
+            {categories.map((c) => {
+              return (
+                <MenuItem key={c.id} value={c.name}>
+                  {c.name}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+        <button
+          className={classes.btn}
+          onClick={() => dispatch(filterByCategory(filter))}
         >
-          <MenuItem value={"*"}>All</MenuItem>
-          {categories.map((c) => {
-            return <MenuItem key={c.id} value={c.name}>{c.name}</MenuItem>;
-          })}
-        </Select>
-      </FormControl>
-      <button onClick={()=>dispatch(filterByCategory(filter))}>Apply</button>
+          Apply
+        </button>
+      </div>
 
-      <TextField
-        id="filled-basic"
-        value={target}
-        onChange={(e) => setTarget(e.target.value)}
-        label="Search"
-        variant="filled"
-      />
-      <button onClick={() => dispatch(search(target))}>Search</button>
+      <div>
+        <TextField
+          id="filled-basic"
+          value={target}
+          onChange={(e) => setTarget(e.target.value)}
+          label="Search"
+          variant="filled"
+        />
+        <button
+          className={classes.btn}
+          onClick={() => dispatch(search(target))}
+        >
+          Search
+        </button>
+      </div>
     </div>
   );
 }
